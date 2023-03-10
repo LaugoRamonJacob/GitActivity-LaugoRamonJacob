61f25df48569991208c1975705e5d219769aa297
LaugoRamonJacob <laugojacob@gmail.com>
2023-03-10 14:36:50 +0800

    My Educational Background added

--- git-python-laugo.py
+++ git-python-laugo.py
@@ -1,3 +1,8 @@
 print("Ramon Jacob Y. Laugo")
 print("Lupac, Boac, Marinduque")
 print("December 29, 2001")
+print("************")
+print("College of Information and Computer Sciences")
+print("BSIT")
+print("2B")
+
